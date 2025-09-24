fff7646552db2c7e77e45481cdc201ad9b7c0790
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-09-24 14:51:38 -0300

    rota login quase finalizada

--- routes/login.py
+++ routes/login.py
@@ -1,9 +1,26 @@
 from flask import Blueprint, request, flash, redirect, url_for, render_template
 
+from backend.validadores.validar_login import validar_login
+
 
 login_bp = Blueprint("login", __name__)
 
 @login_bp.route("/login", methods=["GET", "POST"])
 def login():
+
+    if request.method == "POST":
+
+        nome_completo = request.form.get("nome_completo")
+        senha = request.form.get("senha")
+
+        valido, mensagem = validar_login(nome_completo, senha)
+        if not valido:
+            flash(mensagem, "erro")
+            return render_template("login.html")
+        
+        else:
+            return redirect(url_for(""))
+        
+
     
     return render_template("login.html")
