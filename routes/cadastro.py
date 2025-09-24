cac41eb83fa9e6befddf0b13b2f5d0c02f36193e
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-09-24 14:45:47 -0300

    rota cadastro quase finalizada

--- routes/cadastro.py
+++ routes/cadastro.py
@@ -1,8 +1,36 @@
 from flask import Blueprint, request, flash, redirect, url_for, render_template
 
+from backend.models.usuarios import Usuario
+
+from backend.validadores.validar_cadastro import validar_cadastro
+
+
 cadastro_bp = Blueprint("cadastro", __name__)
 
 @cadastro_bp.route("/cadastro", methods=["GET", "POST"])
 def cadastro():
+
+    nome_completo = request.form.get("nome_completo")
+    senha = request.form.get("senha")
+
+    if request.method == "POST":
+
+        valido, mensagem = validar_cadastro(nome_completo, senha)
+
+        if not valido:
+            flash(mensagem, "erro")
+            return render_template("cadastro.html")
+        
+        else:
+
+            novo_usuario = Usuario(nome_completo, senha)
+
+            novo_usuario.inserir_usuario()
+
+            return redirect(url_for(""))
+
+
+
+
 
     return render_template("cadastro.html")
