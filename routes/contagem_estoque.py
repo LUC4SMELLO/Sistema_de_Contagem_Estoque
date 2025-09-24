8c3105d00da37d2e011423ff9f5cf4f133136e61
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-09-24 16:58:27 -0300

    página contagem estoque

--- routes/contagem_estoque.py
+++ routes/contagem_estoque.py
@@ -3,7 +3,12 @@ from flask import Blueprint, render_template
 produtos = [
     {"id": "8533", "nome": "Coca Cola Lata 350ml"},
     {"id": "9334", "nome": "Coca Cola Zero Lata 350ml"},
-    {"id": "56600", "nome": "Coca Cola 2L"}
+    {"id": "56600", "nome": "Coca Cola 2L"},
+    {"id": "55286", "nome": "Fanta Uva Lata 350ml"},
+    {"id": "55935", "nome": "Coca Cola 600ml"},
+    {"id": "8463", "nome": "Energético Monster Energy 473ml"},
+    {"id": "139441", "nome": "Whiskey Red Label 750ml"},
+    {"id": "7331", "nome": "Água Sem Gás 500ml"}
 ]
 
 contagem_estoque_bp = Blueprint("contagem_estoque", __name__)
