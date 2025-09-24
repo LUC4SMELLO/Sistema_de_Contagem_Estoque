32129b22a79f353e7762d33c85d2f3b8176f5fdb
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-09-24 14:15:08 -0300

    registrando blueprints

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask
 
 from routes.homepage import homepage_bp
+from routes.cadastro import cadastro_bp
+from routes.login import login_bp
 
 app = Flask(__name__)
 app.secret_key = "secret_key"
 
 # REGISTRAR BLUEPRINTS
 app.register_blueprint(homepage_bp)
+app.register_blueprint(cadastro_bp)
+app.register_blueprint(login_bp)
 
 
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
