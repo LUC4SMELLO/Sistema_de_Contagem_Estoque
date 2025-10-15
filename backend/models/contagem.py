fe19d25235d08064b551bb8822b311d185492d8a
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-10-15 15:10:22 -0300

    atualizando campo usuário

--- backend/models/contagem.py
+++ backend/models/contagem.py
@@ -1,9 +1,9 @@
 from database.banco_dados_contagem import conectar_banco_dados_contagem
 
 class Contagem:
-    def __init__(self, data, usuario, codigo_produto, quantidade_contada):
+    def __init__(self, data, usuario_id, codigo_produto, quantidade_contada):
         self.data = data
-        self.usuario = usuario
+        self.usuario_id = usuario_id
         self.codigo_produto = codigo_produto
         self.quantidade_contada = quantidade_contada
 
@@ -16,13 +16,33 @@ class Contagem:
         """
         INSERT INTO TabelaContagem (
         data,
-        usuario,
+        usuario_id,
         codigo_produto,
         quantidade_contada
         )
         VALUES (?, ?, ?, ?)
-        """, (self.data, self.usuario, self.codigo_produto, self.quantidade_contada)
+        """, (self.data, self.usuario_id, self.codigo_produto, self.quantidade_contada)
         )
 
         conexao.commit()
         conexao.close()
+
+    @staticmethod
+    def carregar_contagem(usuario_id):
+
+        conexao = conectar_banco_dados_contagem()
+        cursor = conexao.cursor()
+
+        cursor.execute(
+        """
+        SELECT * FROM TabelaContagem
+        WHERE usuario_id = ?
+        """, (usuario_id,)
+        )
+
+        resultado = cursor.fetchall()
+
+        conexao.commit()
+        conexao.close()
+
+        return Contagem(*resultado)
