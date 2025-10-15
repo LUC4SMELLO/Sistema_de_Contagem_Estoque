5ba98dfb1c61db11e648aa765ee28b2dd50203c5
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-10-15 20:36:05 -0300

    excluindo contagem temporárias

--- routes/salvar_contagem.py
+++ routes/salvar_contagem.py
@@ -1,12 +1,26 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, session
 
 from scripts.processar_arquivo_parametro import lista_produtos
 from scripts.salvar_arquivo_contagem_estoque import salvar_arquivo_contagem_estoque
+
+from backend.models.contagem import Contagem
+
+from datetime import date
+
 
 salvar_contagem_bp = Blueprint("salvar_contagem", __name__)
 
 @salvar_contagem_bp.route("/salvar_contagem", methods=["POST"])
 def salvar_contagem():
+
+    data_atual = date.today()
+    data_atual_formatada = data_atual.strftime("%d/%m/%Y")
+
+    usuario_id = session.get("usuario_id")
+
+    Contagem.excluir_contagem(usuario_id, data_atual_formatada)
+
+
 
     contagens = []
 
