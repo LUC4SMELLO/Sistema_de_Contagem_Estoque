d7dd808e608fc2ef61d6e88a1bfddb6e5ee0935a
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-10-19 15:06:34 -0300

    constantes bancos dados

---
+++ backend/constantes/bancos_dados.py
@@ -0,0 +1,8 @@
+BANCO_DADOS_USUARIOS = "usuarios.db"
+TABELA_USUARIOS = "usuarios"
+
+BANCO_DADOS_PRODUTOS = "produtos.db"
+TABELA_PRODUTOS = "produtos"
+
+BANCO_DADOS_CONTAGENS_TEMPORARIAS = "contagens_temporarias.db"
+TABELA_CONTAGENS_TEMPORARIAS = "contagens_temporarias"
