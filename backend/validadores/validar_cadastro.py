0c42152c85d02b7a15edb50f056de7e67c7d7bc9
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-10-20 16:09:15 -0300

    adicionando docstring

--- backend/validadores/validar_cadastro.py
+++ backend/validadores/validar_cadastro.py
@@ -1,6 +1,23 @@
 from backend.models.usuarios import Usuario
 
-def validar_cadastro(nome_completo, senha):
+def validar_cadastro(nome_completo, senha) -> bool:
+    """
+    Valida as informações do cadastro do usuário.
+
+    Parameters
+    ----------
+        nome_completo
+            O nome completo do usuário.
+        senha
+            A senha do usuário.
+
+    Returns
+    ----------
+        bool
+            True se válido, False caso contrário.
+        mensagem
+            Explicando o motivo do valor booleano.
+    """
 
     resultado = Usuario.buscar_usuarios(nome_completo, senha)
     if resultado:
