a9e788dfca68f1fb811ee83ab56084a377843c99
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-10-20 16:10:36 -0300

    adicionando docstring

--- backend/validadores/validar_login.py
+++ backend/validadores/validar_login.py
@@ -1,6 +1,23 @@
 from backend.models.usuarios import Usuario
 
 def validar_login(nome_completo, senha):
+    """
+    Valida as informações do login do usuário.
+
+    Parameters
+    ----------
+        nome_completo
+            O nome completo do usuário.
+        senha
+            A senha do usuário.
+
+    Returns
+    ----------
+        bool
+            True se válido, False caso contrário.
+        mensagem
+            Explicando o motivo do valor booleano.
+    """
 
     resultado = Usuario.buscar_usuarios(nome_completo, senha)
     if not resultado:
