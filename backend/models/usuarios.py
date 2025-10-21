3e9fa2682e1dba5e54f2e1ea54d6707cd89fdb65
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-10-21 14:46:54 -0300

    adicionando docsstring classe usuário

--- backend/models/usuarios.py
+++ backend/models/usuarios.py
@@ -4,12 +4,39 @@ from backend.constantes.bancos_dados import TABELA_USUARIOS
 
 
 class Usuario:
+    """
+    Representa um usuário.
+
+    Attributes
+    ----------
+        usuario_id
+            O id do usuário.
+        nome_completo
+            O nome completo do usuário.
+        senha
+            A senha do usuário.
+    """
+
     def __init__(self, usuario_id, nome_completo, senha):
+        """
+        Inicializa um novo objeto usuário.
+
+        Parameters
+        ----------
+            usuario_id
+                O id do usuário.
+            nome_completo
+                O nome completo do usuário.
+            senha
+                A senha do usuário.
+        """
+
         self.usuario_id = usuario_id
         self.nome_completo = nome_completo
         self.senha = senha
 
     def inserir_usuario(self):
+        """Insere um usuário no banco de dados."""
 
         conexao = conectar_banco_dados_usuarios()
         cursor = conexao.cursor()
@@ -26,6 +53,19 @@ class Usuario:
 
     @staticmethod
     def excluir_usuario(usuario_id, nome_completo, senha):
+        """
+        Exclui um usuário do banco de dados.
+
+        Parameters
+        ----------
+            usuario_id
+                O id do usuário.
+            nome_completo
+                O nome completo do usuário.
+            senha
+                A senha do usuário.
+        """
+
         
         conexao = conectar_banco_dados_usuarios()
         cursor = conexao.cursor()
@@ -42,6 +82,19 @@ class Usuario:
     
     @staticmethod
     def buscar_usuarios(nome_completo, senha):
+        """
+        Busca um usuário no banco de dados.
+
+        Parameters
+        ----------
+            nome_completo
+                O nome completo do usuário.
+            senha
+                A senha do usuário.
+        Returns
+        -------
+            Um objeto Usuario.
+        """
 
         conexao = conectar_banco_dados_usuarios()
         cursor = conexao.cursor()
