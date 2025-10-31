de11a98e98afa073e8ebd83ea1a41aabf850617b
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-10-31 14:54:53 -0300

    função para inserir contagem final

--- backend/models/contagens.py
+++ backend/models/contagens.py
@@ -1,3 +1,7 @@
+from typing import List, Tuple
+
+from datetime import datetime
+
 from database.banco_dados_contagens import conectar_banco_dados_contagens
 
 from backend.constantes.bancos_dados import TABELA_CONTAGENS
@@ -61,6 +65,52 @@ class Contagens():
 
         conexao.commit()
         conexao.close()
+
+    @staticmethod
+    def inserir_contagens(usuario_id: int, contagens: List[Tuple]):
+        """
+        Insere uma nova contagem no banco de dados.
+
+        Parameters
+        ----------
+            usuario_id: int
+                O id do usuário.
+            contagens: List[Tuple]
+                Uma lista de tuplas com as contagens.
+
+        Returns
+        -------
+            None
+        """
+        
+        data_atual = datetime.now()
+        data_atual_formatada = data_atual.strftime("%d/%m/%Y, %H:%M")
+
+        conexao = conectar_banco_dados_contagens()
+        cursor = conexao.cursor()
+
+        for i in range(len(contagens)):
+            cursor.execute(
+                f"""
+                INSERT INTO {TABELA_CONTAGENS} (
+                data,
+                usuario_id,
+                codigo_produto,
+                quantidade_contada
+                )
+                VALUES (?, ?, ?, ?)
+                """,
+                (
+                    data_atual_formatada,
+                    usuario_id,
+                    contagens[i]["codigo"],
+                    contagens[i]["quantidade"]
+                )
+            )
+
+        conexao.commit()
+        conexao.close()
+
 
     @staticmethod
     def excluir_contagem(usuario_id, data):
