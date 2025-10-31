b85d59805097371aafb45ac5b44d4681db2f9425
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-10-31 11:08:30 -0300

    adicionando banco dados contagens

--- backend/constantes/bancos_dados.py
+++ backend/constantes/bancos_dados.py
@@ -4,5 +4,8 @@ TABELA_USUARIOS = "usuarios"
 BANCO_DADOS_PRODUTOS = "produtos.db"
 TABELA_PRODUTOS = "produtos"
 
+BANCO_DADOS_CONTAGENS = "contagens.db"
+TABELA_CONTAGENS = "contagens"
+
 BANCO_DADOS_CONTAGENS_TEMPORARIAS = "contagens_temporarias.db"
 TABELA_CONTAGENS_TEMPORARIAS = "contagens_temporarias"
