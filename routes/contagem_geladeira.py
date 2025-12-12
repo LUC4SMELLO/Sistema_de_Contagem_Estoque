0646dd638df61567d95dd3836a3c81d4aad6d40d
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-12-12 15:39:27 -0300

    correção render_template

--- routes/contagem_geladeira.py
+++ routes/contagem_geladeira.py
@@ -5,4 +5,4 @@ contagem_geladeira_bp = Blueprint("contagem_geladeira", __name__)
 @contagem_geladeira_bp.route("/contagem_geladeira", methods=["GET"])
 def contagem_geladeira():
 
-    return render_template("contagem_geladeira.html")
+    return render_template("contagem_geladeiras.html")
