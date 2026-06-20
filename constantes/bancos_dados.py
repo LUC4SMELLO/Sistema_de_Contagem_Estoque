8b2928ee6bf9343e45d5685c65c3dce1d7567d85
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-06-19 21:06:47 -0300

    feat: mudando diretórios

---
+++ constantes/bancos_dados.py
@@ -0,0 +1,12 @@
+from constantes.paths import DATABASE_DIR
+
+
+BANCO_DADOS_PRINCIPAL = DATABASE_DIR / "app.db"
+
+TABELA_USUARIOS = "usuarios"
+
+TABELA_PRODUTOS = "produtos"
+
+TABELA_CONTAGENS = "contagens"
+
+TABELA_CONTAGENS_TEMPORARIAS = "contagens_temporarias"
---
+++ constantes/paths.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATABASE_DIR = BASE_DIR / "database"
