9f9d5f7740e23b3e2733fe817bc9cdb6ecdcf384
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-06-19 21:08:02 -0300

    feat: mudando diretórios

---
+++ models/contagem_temporaria.py
@@ -0,0 +1,125 @@
+from database.banco_dados_principal import conectar_banco_dados_principal
+
+from constantes.bancos_dados import TABELA_CONTAGENS_TEMPORARIAS
+
+
+class ContagemTemporaria:
+    """
+    Representa a contagem temporária feita pelo usuário.
+
+    Attributes
+    ----------
+        data
+            Data da contagem.
+        usuario_id
+            Id do usuário.
+        codigo_produto
+            Código produto.
+        quantidade_contada
+            Quantidade Contada.
+    """
+
+    def __init__(self, data, usuario_id, codigo_produto, quantidade_contada):
+        """
+        Inicializa uma nova contagem temporária.
+
+        Parameters
+        ----------
+            data
+                A data da contagem.
+            usuario_id
+                O id do usuário.
+            codigo_produto
+                O código do produto.
+            quantidade_contada
+                A quantidade contada.
+        """
+
+        self.data = data
+        self.usuario_id = usuario_id
+        self.codigo_produto = codigo_produto
+        self.quantidade_contada = quantidade_contada
+
+    def inserir_contagem(self):
+        """
+        Insere uma nova contagem temporária no banco de dados.
+        """
+
+        conexao = conectar_banco_dados_principal()
+        cursor = conexao.cursor()
+
+        cursor.execute(
+        f"""
+        INSERT INTO {TABELA_CONTAGENS_TEMPORARIAS} (
+        data,
+        usuario_id,
+        codigo_produto,
+        quantidade_contada
+        )
+        VALUES (?, ?, ?, ?)
+        """, (self.data, self.usuario_id, self.codigo_produto, self.quantidade_contada)
+        )
+
+        conexao.commit()
+        conexao.close()
+
+    @staticmethod
+    def excluir_contagem(usuario_id, data):
+        """
+        Exclui a contagem temporária do banco de dados.
+
+        Parameters
+        ----------
+            usuario_id
+                O id do usuário.
+            data
+                A data da contagem temporária.
+        """
+
+        conexao = conectar_banco_dados_principal()
+        cursor = conexao.cursor()
+
+        cursor.execute(
+        f"""
+        DELETE FROM {TABELA_CONTAGENS_TEMPORARIAS}
+        WHERE usuario_id = ? AND data = ?
+        """, (usuario_id, data)
+        )
+
+        conexao.commit()
+        conexao.close()
+
+    @staticmethod
+    def carregar_contagem(usuario_id, data):
+        """
+        Carrega a contagem temporária do banco de dados.
+
+        Parameters
+        ----------
+            usuario_id
+                O id do usuário.
+            data
+                A data da contagem temporária.
+
+        Returns
+        -------
+            Uma lista de tuplas.
+        """
+
+        conexao = conectar_banco_dados_principal()
+        cursor = conexao.cursor()
+
+        cursor.execute(
+        f"""
+        SELECT * FROM {TABELA_CONTAGENS_TEMPORARIAS}
+        WHERE usuario_id = ? AND data = ?
+        """, (usuario_id, data)
+        )
+
+        resultado = cursor.fetchall()
+
+        conexao.close()
+
+        contagens = [{"produto_id": linha[2], "quantidade": linha[3]} for linha in resultado]
+
+        return contagens
---
+++ models/usuarios.py
@@ -0,0 +1,111 @@
+from database.banco_dados_principal import conectar_banco_dados_principal
+
+from constantes.bancos_dados import TABELA_USUARIOS
+
+
+class Usuario:
+    """
+    Representa um usuário.
+
+    Attributes
+    ----------
+        usuario_id : int | None
+            O ID do usuário (gerado automaticamente pelo banco).
+        nome_completo : str
+            O nome completo do usuário.
+        senha : str
+            A senha do usuário.
+    """
+
+
+    def __init__(self, nome_completo: str, senha: str, usuario_id=None):
+        """
+        Inicializa um novo objeto usuário.
+
+        Parameters
+        ----------
+            nome_completo : str
+                O nome completo do usuário.
+            senha : str
+                A senha do usuário.
+            usuario_id : int, opcional
+                O ID do usuário (usado apenas ao buscar do banco).
+        """
+        self.usuario_id = usuario_id
+        self.nome_completo = nome_completo
+        self.senha = senha
+
+
+    def inserir_usuario(self):
+        """Insere um novo usuário no banco de dados e atualiza o ID no objeto."""
+
+        conexao = conectar_banco_dados_principal()
+        cursor = conexao.cursor()
+
+        cursor.execute(
+            f"""
+            INSERT INTO {TABELA_USUARIOS} (nome_completo, senha)
+            VALUES (?, ?)
+            """,
+            (self.nome_completo, self.senha),
+        )
+
+        self.usuario_id = cursor.lastrowid
+
+        conexao.commit()
+        conexao.close()
+
+
+    @staticmethod
+    def excluir_usuario(usuario_id: int):
+        """
+        Exclui um usuário do banco de dados.
+
+        Parameters
+        ----------
+            usuario_id : int
+                O ID do usuário a ser excluído.
+        """
+        conexao = conectar_banco_dados_principal()
+        cursor = conexao.cursor()
+
+        cursor.execute(
+            f"DELETE FROM {TABELA_USUARIOS} WHERE usuario_id = ?", (usuario_id,)
+        )
+
+        conexao.commit()
+        conexao.close()
+    
+
+    @staticmethod
+    def buscar_usuarios(nome_completo, senha):
+        """
+        Busca um usuário no banco de dados.
+
+        Parameters
+        ----------
+            nome_completo
+                O nome completo do usuário.
+            senha
+                A senha do usuário.
+        Returns
+        -------
+            Um objeto Usuario.
+        """
+
+        conexao = conectar_banco_dados_principal()
+        cursor = conexao.cursor()
+
+        cursor.execute(
+        f"""
+        SELECT * FROM {TABELA_USUARIOS}
+        WHERE nome_completo = ? AND senha = ?
+        """, (nome_completo, senha)
+        )
+
+        resultado = cursor.fetchone()
+
+        conexao.commit()
+        conexao.close()
+
+        return resultado
