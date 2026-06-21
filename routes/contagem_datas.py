4cc43fbb3ed363ba1d2ad30a479839b1255eb512
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-06-21 12:22:39 -0300

    feat: salvando contagem no banco de dados

--- routes/contagem_datas.py
+++ routes/contagem_datas.py
@@ -2,22 +2,11 @@ from flask import Blueprint, render_template
 
 from constantes.layout_estoque import enderecos
 
-lista_produtos = [
-    {"nome": "Coca Cola 2L"},
-    {"nome": "Coca 350ml"},
-    {"nome": "Coca Zero 2L"},
-    {"nome": "Coca Cola Sem Açúcar 350ml"},
-    {"nome": "Coca Cola Garrafa 600ml"},
-    {"nome": "Coca Cola Sem Açúcar 600ml"},
-    {"nome": "Coca Cola Café 220ml"},
-    {"nome": "Coca Cola Cherry 355ml"},
-    {"nome": "Fanta Laranja 2L"},
-    {"nome": "Sprite 2L"}
-]
+from constantes.lista_produtos import bebidas
 
 
 contagem_datas_bp = Blueprint("contagem_datas", __name__)
 
 @contagem_datas_bp.route("/contagem_datas", methods=["GET", "POST"])
 def contagem_datas():
-    return render_template("contagem_datas.html", layout_estoque=enderecos, lista_produtos=lista_produtos)
+    return render_template("contagem_datas.html", layout_estoque=enderecos, lista_produtos=bebidas)
--- routes/salvar_contagem_datas.py
+++ routes/salvar_contagem_datas.py
@@ -1,8 +1,19 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, session, render_template
+
+from models.contagens_datas import ContagensDatas
+
+from constantes.lista_produtos import bebidas
+
+
 salvar_contagem_datas_bp = Blueprint("salvar_contagem_datas", __name__)
 
 @salvar_contagem_datas_bp.route('/salvar_contagem_datas', methods=['POST'])
 def salvar_contagem_datas():
+
+    usuario_id = session.get("usuario_id")
+
+    contagens = []
+
     # Captura as listas de cada coluna enviada pelo formulário
     ruas = request.form.getlist('rua[]')
     blocos = request.form.getlist('bloco[]')
@@ -11,13 +22,25 @@ def salvar_contagem_datas():
     produtos = request.form.getlist('produto[]')
     datas = request.form.getlist('data[]')
 
+
+    produtos_dict = {
+        str(produto["codigo"]): produto["nome"]
+        for produto in bebidas
+    }
+
     # Processa os dados linha por linha usando o zip do Python
     for r, b, c, n, prod, dt in zip(ruas, blocos, colunas, niveis, produtos, datas):
-        # Ignora linhas onde o produto ou a data não foram preenchidos
-        if not prod or not dt:
-            continue
-            
-        print(f"Salvando: Rua {r}, Bloco {b}, Coluna {c}, Nível {n} -> Produto: {prod} em {dt}")
-        # Aqui você insere a lógica para salvar no seu Banco de Dados
 
-    return "Contagem salva com sucesso!"
+        contagens.append({
+            "rua": r,
+            "bloco": b,
+            "coluna": c,
+            "nivel": n,
+            "codigo_produto": prod,
+            "nome": produtos_dict.get(prod),
+            "data_contada": dt
+        })
+            
+    ContagensDatas.inserir_contagens(usuario_id, contagens)
+
+    return render_template("salvar_contagem.html")
