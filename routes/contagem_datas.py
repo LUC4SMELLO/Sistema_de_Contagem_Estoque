36807dc9a65bb9e12111e499e20c390c62b28a3f
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-06-21 15:31:57 -0300

    feat: carregando contagem anterior

--- routes/contagem_datas.py
+++ routes/contagem_datas.py
@@ -1,12 +1,20 @@
 from flask import Blueprint, render_template
 
-from constantes.layout_estoque import enderecos
+from models.contagens_datas import ContagensDatas
 
+from constantes.layout_estoque import enderecos
 from constantes.lista_produtos import bebidas
 
+
+contagem_anterior = ContagensDatas.buscar_ultima_contagem()
 
 contagem_datas_bp = Blueprint("contagem_datas", __name__)
 
 @contagem_datas_bp.route("/contagem_datas", methods=["GET", "POST"])
 def contagem_datas():
-    return render_template("contagem_datas.html", layout_estoque=enderecos, lista_produtos=bebidas)
+    return render_template(
+        "contagem_datas.html",
+        layout_estoque=enderecos,
+        contagem_anterior=contagem_anterior,
+        lista_produtos=bebidas
+        )
