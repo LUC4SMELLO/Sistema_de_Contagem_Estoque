0faea0078dd9f43682725706a71c1520555100a5
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-06-24 15:13:26 -0300

    feat: mudança de estrutura 'archives/exports/'

--- constants/paths.py
+++ constants/paths.py
@@ -4,3 +4,16 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASE_DIR = BASE_DIR / "database"
+
+ARCHIVES = BASE_DIR / "archives"
+
+EXPORTS = ARCHIVES / "exports"
+
+
+EXPORT_CONTAGEM_ESTOQUE = EXPORTS / "contagem_estoque"
+
+EXPORT_CONTAGEM_ESTOQUE_DOCES = EXPORTS / "contagem_estoque_doces"
+
+EXPORT_CONTAGEM_DATAS = EXPORTS / "contagem_datas"
+
+EXPORT_CONTAGEM_GELADEIRAS = EXPORTS / "contagem_geladeiras"
