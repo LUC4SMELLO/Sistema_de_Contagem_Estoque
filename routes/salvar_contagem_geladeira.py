cc212ecc4384f839255980e23aa0cf8c782a9e71
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-06-28 13:59:51 -0300

    feat: adicionando verificação ao salvar contagem

--- routes/salvar_contagem_geladeira.py
+++ routes/salvar_contagem_geladeira.py
@@ -1,8 +1,4 @@
-from flask import Blueprint, request, session, jsonify
-from datetime import date
-import pandas as pd
-
-from models.contagem_temporaria import ContagemTemporaria
+from flask import Blueprint, request, session, jsonify, render_template
 
 from scripts.salvar_arquivo_contagem_geladeiras import salvar_arquivo_contagem_geladeiras
 
@@ -11,10 +7,6 @@ salvar_contagem_geladeiras_bp = Blueprint("salvar_contagem_geladeiras", __name__
 
 @salvar_contagem_geladeiras_bp.route("/salvar_contagem_geladeiras", methods=["GET", "POST"])
 def salvar_contagem_geladeiras():
-
-
-    data_atual = date.today()
-    data_formatada = data_atual.strftime("%d/%m/%Y")
 
     usuario_id = session.get("usuario_id", "desconhecido")
 
@@ -32,9 +24,7 @@ def salvar_contagem_geladeiras():
             "quantidade": 1
         })
 
-    salvar_arquivo_contagem_geladeiras(contagens)
-
-    ContagemTemporaria.excluir_contagem(usuario_id)
+    resultado, mensagem, erro = salvar_arquivo_contagem_geladeiras(contagens, usuario_id)
 
 
     return jsonify({"status": "ok"})
--- scripts/salvar_arquivo_contagem_geladeiras.py
+++ scripts/salvar_arquivo_contagem_geladeiras.py
@@ -1,11 +1,16 @@
+import os
 from typing import List, Tuple
+
+from datetime import datetime
 
 import pandas as pd
 
 from constants.paths import EXPORT_CONTAGEM_GELADEIRAS
 
+from models.contagem_temporaria import ContagemTemporaria
 
-def salvar_arquivo_contagem_geladeiras(contagens: List[Tuple]) -> None:
+
+def salvar_arquivo_contagem_geladeiras(contagens: List[Tuple], usuario_id) -> None:
     """
     Salva um arquivo .csv com a contagem de geladeiras feita pelo usuário.
 
@@ -19,6 +24,25 @@ def salvar_arquivo_contagem_geladeiras(contagens: List[Tuple]) -> None:
         None
     """
 
-    contagem_geladeiras = pd.DataFrame(contagens)
+    try:
 
-    contagem_geladeiras.to_csv(EXPORT_CONTAGEM_GELADEIRAS / "contagem_geladeiras.csv", index=False)
+        data_atual = datetime.now()
+        data_atual_formatada_arquivo = data_atual.strftime("%d-%m-%Y_%H-%M-%S")
+
+        contagem_geladeiras = pd.DataFrame(contagens)
+
+        CAMINHO_SALVAR_ARQUIVO_CSV = EXPORT_CONTAGEM_GELADEIRAS / f"contagem_geladeiras_{data_atual_formatada_arquivo}.csv"
+
+        contagem_geladeiras.to_csv(CAMINHO_SALVAR_ARQUIVO_CSV, index=False)
+
+
+        if not os.path.exists(CAMINHO_SALVAR_ARQUIVO_CSV) or os.path.getsize(CAMINHO_SALVAR_ARQUIVO_CSV) == 0:
+            raise FileNotFoundError("Falha crítica: O arquivo não foi criado ou está vazio.")
+
+
+        ContagemTemporaria.excluir_contagem(usuario_id)
+
+        return True, "<span class='mensagem-sucesso'>Sucesso ao Enviar a Contagem!</span>", ""
+
+    except Exception as erro:
+        return False, "<span class='mensagem-erro'>Erro ao Enviar a Contagem!</span>", erro
