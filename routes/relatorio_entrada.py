2500c763586051f1c464b201ece0d5c9b4ce716e
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-07-28 12:55:54 -0300

    feat: enviando dados da nota fiscal para visualização

--- routes/relatorio_entrada.py
+++ routes/relatorio_entrada.py
@@ -1,8 +1,16 @@
 from flask import Blueprint, render_template
 
+from teste_xml import ler_arquivo_xml
+from constants.paths import IMPORTS
+
+CAMINHO_XML = IMPORTS / "50_13633613_Luis_FDB7977_Betim.xml"
+
 
 relatorio_entrada_bp = Blueprint("relatorio_entrada", __name__)
 
 @relatorio_entrada_bp.route("/relatorio_entrada")
 def relatorio_entrada():
-    return render_template("relatorio_entrada.html")
+
+    dados = ler_arquivo_xml(CAMINHO_XML)
+
+    return render_template("relatorio_entrada.html", nota=dados)
