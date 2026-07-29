976e5a9af00ce02ed4215f14635bfc542cda5780
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-07-29 09:53:09 -0300

    feat: melhorando sistema de cargas

--- routes/relatorio_entrada.py
+++ routes/relatorio_entrada.py
@@ -1,16 +1,27 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from scripts.ler_arquivo_xml import ler_arquivo_xml
 from constants.paths import IMPORTS
 
-CAMINHO_XML = IMPORTS / "50_13633613_Luis_FDB7977_Betim.xml"
+
+cargas = {
+    1: IMPORTS / "1_13633613_Luís_FDB7977_Betim.xml",
+    2: IMPORTS / "2_97190_André_FHW5858_Itabirito.xml",
+}
 
 
 relatorio_entrada_bp = Blueprint("relatorio_entrada", __name__)
 
-@relatorio_entrada_bp.route("/relatorio_entrada")
+@relatorio_entrada_bp.route("/relatorio_entrada", methods=["GET", "POST"])
 def relatorio_entrada():
 
-    dados = ler_arquivo_xml(CAMINHO_XML)
+    dados = None
+    carga_selecionada = None
 
-    return render_template("relatorio_entrada.html", nota=dados)
+    if request.method == "POST":
+        carga_selecionada = request.form["carga"]
+
+        caminho_xml = cargas[int(carga_selecionada)]
+        dados = ler_arquivo_xml(caminho_xml)
+
+    return render_template("relatorio_entrada.html", cargas=cargas, nota=dados, carga_selecionada=carga_selecionada)
