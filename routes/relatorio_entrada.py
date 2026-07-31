61eac396bb00ccadf30f9414d59b300eb863be30
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-07-31 11:52:27 -0300

    feat: ajustando entradas dos dados das cargas

--- routes/relatorio_entrada.py
+++ routes/relatorio_entrada.py
@@ -1,19 +1,14 @@
 from flask import Blueprint, render_template, request
 
-from scripts.ler_arquivo_xml import ler_varios_arquivos_xml
-from constants.paths import IMPORTS
-
-
-cargas = {
-    1: IMPORTS / "1_97190_André_FHW5858_Itabirito.xml",
-    2: IMPORTS / "1_13633613_André_FHW5858_Itabirito.xml",
-}
+from scripts.ler_arquivo_xml import ler_varios_arquivos_xml, retornar_numeros_das_cargas
 
 
 relatorio_entrada_bp = Blueprint("relatorio_entrada", __name__)
 
 @relatorio_entrada_bp.route("/relatorio_entrada", methods=["GET", "POST"])
 def relatorio_entrada():
+
+    cargas = retornar_numeros_das_cargas()
 
     dados = None
     carga_selecionada = None
